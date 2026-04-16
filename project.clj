5a2afc49f14c047ce80fec2f9898d67ec4a5b136
ruroru <111705692+ruroru@users.noreply.github.com>
2026-04-16 00:29:19 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/tassu "1.0.3"
+(defproject org.clojars.jj/tassu "1.0.4-SNAPSHOT"
   :description "A routing library for clojure ring"
   :url "https://github.com/ruroru/tassu"
   :license {:name "EPL-2.0"
