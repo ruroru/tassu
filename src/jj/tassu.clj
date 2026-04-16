7c10519ab198c9e2a6836f1046529918df6eac3f
jj <keistaslogairkosulys@gmail.com>
2026-04-16 02:10:03 +0200

    use deftype instead of defrecord for ParamRoute

--- src/jj/tassu.clj
+++ src/jj/tassu.clj
@@ -1,33 +1,22 @@
-(ns jj.tassu
-  (:require [clojure.string :as str])
-  (:import (java.util.regex Pattern)))
+(ns jj.tassu)
 
-(defrecord ParamRoute [pattern param-keys method handler specificity])
+(deftype ParamRoute [segments param-keys method handler specificity])
 
-(defn- parse-path-segments
-  [path]
-  (loop [chars (seq path)
-         current-segment []
-         segments []]
-    (cond
-      (empty? chars)
-      (if (empty? current-segment)
-        segments
-        (conj segments (apply str current-segment)))
+(defn- fast-split
+  [^String uri]
+  (let [len (.length uri)]
+    (if (or (<= len 1) (not (.startsWith uri "/")))
+      []
+      (loop [start 1
+             res (transient [])]
+        (let [end (.indexOf uri "/" start)]
+          (if (== end -1)
+            (persistent! (conj! res (.substring uri start len)))
+            (recur (unchecked-inc end)
+                   (conj! res (.substring uri start end)))))))))
 
-      (= (first chars) \/)
-      (if (empty? current-segment)
-        (recur (rest chars) [] segments)
-        (recur (rest chars) [] (conj segments (apply str current-segment))))
-
-      :else
-      (recur (rest chars) (conj current-segment (first chars)) segments))))
-
-(defn- segment-starts-with-colon? [segment]
-  (and (seq segment) (= (first segment) \:)))
-
-(defn- extract-param-name [segment]
-  (apply str (rest segment)))
+(defn- segment-starts-with-colon? [^String segment]
+  (and (not (.isEmpty segment)) (= (.charAt segment 0) \:)))
 
 (defn- route-specificity [segments]
   (reduce (fn [score segment]
@@ -37,26 +26,23 @@
           (count segments)
           segments))
 
-(defn- compile-param-pattern [segments]
-  (let [param-keys (transient [])
-        pattern-parts (mapv (fn [segment]
-                              (if (segment-starts-with-colon? segment)
-                                (do
-                                  (conj! param-keys (keyword (extract-param-name segment)))
-                                  "([^/]+)")
-                                (Pattern/quote segment)))
-                            segments)
-        pattern-str (if (empty? pattern-parts)
-                      "^/$"
-                      (str "^/" (str/join "/" pattern-parts) "$"))]
-    {:pattern     (re-pattern pattern-str)
+(defn- analyze-route-path [path]
+  (let [raw-segments (fast-split path)
+        param-keys (transient [])
+        segments (mapv (fn [seg]
+                         (if (segment-starts-with-colon? seg)
+                           (do (conj! param-keys (keyword (.substring ^String seg 1)))
+                               nil)
+                           seg))
+                       raw-segments)]
+    {:segments    segments
      :param-keys  (persistent! param-keys)
-     :specificity (route-specificity segments)}))
+     :specificity (route-specificity raw-segments)}))
 
 (defn- create-static-cache [route-specs]
   (reduce
     (fn [cache [path handlers]]
-      (let [segments (parse-path-segments path)]
+      (let [segments (fast-split path)]
         (if (some segment-starts-with-colon? segments)
           cache
           (reduce
@@ -70,42 +56,49 @@
 (defn- create-param-routes [route-specs]
   (->> route-specs
        (filter (fn [[path _]]
-                 (some segment-starts-with-colon? (parse-path-segments path))))
+                 (some segment-starts-with-colon? (fast-split path))))
        (mapcat (fn [[path handlers]]
-                 (let [segments (parse-path-segments path)
-                       {:keys [pattern param-keys specificity]} (compile-param-pattern segments)]
+                 (let [{:keys [segments param-keys specificity]} (analyze-route-path path)]
                    (map (fn [handler]
-                          (->ParamRoute pattern param-keys (:method handler)
-                                        (:handler handler) specificity))
+                          (ParamRoute. segments param-keys (:method handler)
+                                       (:handler handler) specificity))
                         handlers))))
-       (sort-by :specificity >)
+       (sort-by (fn [^ParamRoute r] (.-specificity r)) >)
        vec))
 
-(defn- match-param-route [param-route uri method]
-  (when (identical? method (:method param-route))
-    (when-let [matches (re-matches (:pattern param-route) uri)]
-      (let [param-values (rest matches)]
-        {:handler (:handler param-route)
-         :params  (zipmap (:param-keys param-route) param-values)}))))
+(defn- match-param-route [^ParamRoute route uri-segments method]
+  (let [r-segments (.-segments route)]
+    (when (and (identical? method (.-method route))
+               (== (count r-segments) (count uri-segments)))
+      (let [p-keys (.-param-keys route)
+            p-count (count p-keys)
+            ^objects p-values (make-array Object p-count)]
+        (loop [i 0
+               pk-idx 0]
+          (if (< i (count r-segments))
+            (let [r-seg (nth r-segments i)
+                  u-seg (nth uri-segments i)]
+              (if (nil? r-seg)
+                (do
+                  (aset p-values pk-idx u-seg)
+                  (recur (unchecked-inc i) (unchecked-inc pk-idx)))
+                (if (.equals ^String r-seg u-seg)
+                  (recur (unchecked-inc i) pk-idx)
+                  nil)))
+            (zipmap p-keys p-values)))))))
 
-(defn- find-param-route-match [param-routes uri method]
+(defn- find-param-route-match [param-routes uri-segments method]
   (loop [routes param-routes]
-    (when-let [route (first routes)]
-      (if-let [match (match-param-route route uri method)]
-        match
-        (recur (rest routes))))))
+    (if-let [route (first routes)]
+      (if-let [params (match-param-route route uri-segments method)]
+        {:handler (.-handler ^ParamRoute route) :params params}
+        (recur (rest routes)))
+      nil)))
 
 (def ^:private not-found
   {:status  404
    :body    "Not found"
    :headers {"content-type" "text/html"}})
-
-(defn- resolve-match [static-cache param-routes request]
-  (let [method (:request-method request)
-        uri    (:uri request)]
-    (if-let [handler (get-in static-cache [uri method])]
-      {:handler handler :params {}}
-      (find-param-route-match param-routes uri method))))
 
 (defn route
   [route-specs]
@@ -114,43 +107,31 @@
     (fn [request]
       (let [method (:request-method request)
             uri (:uri request)]
-
         (if-let [static-handler (get-in static-cache [uri method])]
           (static-handler (assoc request :params {}))
-
-          (if-let [param-match (find-param-route-match param-routes uri method)]
-            ((:handler param-match) (assoc request :params (:params param-match)))
-
-            {:status  404
-             :body    "Not found"
-             :headers {"content-type" "text/html"}}))))))
+          (let [uri-segments (fast-split uri)]
+            (if-let [match (find-param-route-match param-routes uri-segments method)]
+              ((:handler match) (assoc request :params (:params match)))
+              not-found)))))))
 
 (defn async-route
   [route-specs]
   (let [static-cache (create-static-cache route-specs)
         param-routes (create-param-routes route-specs)]
     (fn [request respond raise]
-      (if-let [{:keys [handler params]} (resolve-match static-cache param-routes request)]
-        (handler (assoc request :params params) respond raise)
-        (respond not-found)))))
+      (let [method (:request-method request)
+            uri (:uri request)]
+        (if-let [static-handler (get-in static-cache [uri method])]
+          (static-handler (assoc request :params {}) respond raise)
+          (let [uri-segments (fast-split uri)]
+            (if-let [match (find-param-route-match param-routes uri-segments method)]
+              ((:handler match) (assoc request :params (:params match)) respond raise)
+              (respond not-found))))))))
 
-(defn- create-route [method handler]
-  {:method method :handler handler})
-
-(defn GET [handler]
-  (create-route :get handler))
-
-(defn HEAD [handler]
-  (create-route :head handler))
-
-(defn PUT [handler]
-  (create-route :put handler))
-
-(defn PATCH [handler]
-  (create-route :patch handler))
-
-(defn POST [handler]
-  (create-route :post handler))
-
-(defn DELETE [handler]
-  (create-route :delete handler))
+(defn- create-route [method handler] {:method method :handler handler})
+(defn GET [h] (create-route :get h))
+(defn HEAD [h] (create-route :head h))
+(defn PUT [h] (create-route :put h))
+(defn PATCH [h] (create-route :patch h))
+(defn POST [h] (create-route :post h))
+(defn DELETE [h] (create-route :delete h))
